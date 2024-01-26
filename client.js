3f4aece65366c2d963cfcda0e20bbca0cd3bca4a
David Smith <djsmith611@me.com>
2024-01-25 20:00:16 -0700

    completed assignment

--- client.js
+++ client.js
@@ -6,6 +6,20 @@
  * 3. After the loop, log out the total number of
  *    hobbies
  */
+let hobbies=['hockey', 'basketball', 'coding'];
+console.log(hobbies);
+console.log('for of loop');
+
+
+for (let i of hobbies){
+     console.log(i);
+}
+
+for (let i=0; i<hobbies.length; i++){
+     console.log(hobbies[i]);
+}
+
+console.log(hobbies.length);
 
 
 // Example output
@@ -25,6 +39,27 @@
  * 4. Output the array and number of times teal was found
  */
 
+let colors =['teal', 'black', 'teal', 'blue', 'red']
+let tealCount =0 ;
+let index=colors.length -1;
+
+while(index>=0){
+     if(colors[index]==='teal'){
+          console.log('This is teal.');
+          tealCount += 1; 
+          index -= 1;
+          console.log('Teal count ', tealCount);
+     } else {
+          console.log('This is not teal.');
+          index -=1;
+          console.log('Teal count ', tealCount);
+     }
+}
+
+
+
+
+
 
 // Example output
 // green, red, teal, orange, teal
@@ -41,6 +76,22 @@
  * 4. Output the original array, odd number array and even number array
  */
 
+let numbers=[1,2,3,4,5,6,7,8,9,10]
+
+let oddNumbers =[];
+let evenNumbers =[];
+
+for (let teal of numbers){
+     if(teal % 2 === 0){
+          evenNumbers.push(teal);
+     } else {
+          oddNumbers.push(teal);
+     }
+}
+console.log('original array: ', numbers);
+console.log('Even Numbers: ', evenNumbers);
+console.log('Odd Numbers: ', oddNumbers);
+
 
 // Example output
 // 3, 7, 2, 8, 11, 4, 2
@@ -56,6 +107,20 @@
  * 3. Write a loop that adds the opposite value to the toggled array
  * 4. Output both arrays
  */
+let switchBoard = [true,false,true,true,false,false,false,false,true,true,true];
+
+let toggled = [];
+
+for (let i of switchBoard){
+     if(i === true){
+          toggled.push(false);
+     } else{
+          toggled.push(true);
+     }
+}
+
+console.log(switchBoard);
+console.log(toggled);
 
 
 // Example output
@@ -75,6 +140,13 @@
  * Hint: Try using a while loop for this one.
  */
 
+let f=[641,61,0,45654,46,46,484,565,4365,0,4654,64,846,0,465,4635,4654,64,65198,461265,1984,97,74,6158,7,4,91,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+
+while (f.length > 0 && f[f.length - 1] === 0){
+     f.pop();
+}
+ 
+console.log(f);
 
 // Example output
 // Before loop 3, 0, 2, 8, 0, 0, 0
@@ -90,6 +162,37 @@
       and lowest numbers.
  * 3. Log the highest and lowest number to the console
  */
+
+let largishArray = [5,4,7,8,1,4,5,3,2,5,4,85,5,52,12,1,5,5,5,6,6,9,1,454,5,1,5,5,4,558,5,5,51,5,85,4,4,5,85,4,5,85,51,5,,56,56,58,9,5,5,9,5,54,5,69,88,5,55,9,89,85,5,69,98,5,5,69,9,8,56,9,9,5,5,69,9,85,5,6,9,9,5,566,9,89,89,8,898,56,5,5985,985,95,985,9859]
+
+let high = 0;
+
+let low = Number.MAX_SAFE_INTEGER;
+console.log('for .. of loop');
+for (let i of largishArray){
+     if (i>high){
+          high = i;
+     } else if(i<low){
+          low = i;
+     } 
+}
+
+console.log(high);
+console.log(low);
+//
+//
+//
+//console.log('3 part for loop');
+//for (let i=0; i<=largishArray.length -1; i++){
+ //    if (i>high){
+   //       high = largishArray[i];
+     //} else if(i<low){
+       //   low = largishArray[i];
+     //}
+//}
+//console.log(high);
+//console.log(low);
+
 
 
 // Example output
